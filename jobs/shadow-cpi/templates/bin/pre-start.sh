02e78ffd84be9ee5adee6014cc94345d200aa5a8
Benjamin Gandon <benjamin@gstack.io>
2018-09-12 16:08:10 +0200

    Fix variable dereference syntax

--- jobs/shadow-cpi/templates/bin/pre-start.sh
+++ jobs/shadow-cpi/templates/bin/pre-start.sh
@@ -16,7 +16,7 @@ chmod 600 /var/vcap/jobs/shadow-cpi/config/id_rsa
 # Accept the remote ECDSA host key
 remote_host=<%= esc(p('ssh.remote-host')) %>
 if [[ -f ~/.ssh/known_hosts ]]; then
-    ssh-keygen -f ~/.ssh/known_hosts -R "$(remote_host)"
+    ssh-keygen -f ~/.ssh/known_hosts -R "${remote_host}"
 fi
-ssh-keyscan -t ecdsa "$(remote_host)" 2> /dev/null \
+ssh-keyscan -t ecdsa "${remote_host}" 2> /dev/null \
     >> ~/.ssh/known_hosts
