5c7bf90f062de0b66be5bcc2bc56fbc9d5ae1526
Benjamin Gandon <benjamin@gstack.io>
2018-09-12 16:30:38 +0200

    Create the '~/.ssh' if necessary

--- jobs/shadow-cpi/templates/bin/pre-start.sh
+++ jobs/shadow-cpi/templates/bin/pre-start.sh
@@ -18,5 +18,7 @@ remote_host=<%= esc(p('ssh.remote-host')) %>
 if [[ -f ~/.ssh/known_hosts ]]; then
     ssh-keygen -f ~/.ssh/known_hosts -R "${remote_host}"
 fi
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
 ssh-keyscan -t ecdsa "${remote_host}" 2> /dev/null \
     >> ~/.ssh/known_hosts
